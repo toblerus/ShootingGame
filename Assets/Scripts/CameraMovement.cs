ad0670e583e9200f0cc63a5468cee07d6470455f
Tobi <toblerus@hotmail.de>
2018-10-08 21:06:49 +0200

    Added everything

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private GameObject _playerBody;
+    [SerializeField] private GameObject _mainCamera;
+
+    void Update ()
+    {
+        _mainCamera.transform.eulerAngles = new Vector3(90, 0, 0);
+        _mainCamera.transform.position = new Vector3(_playerBody.transform.position.x, 1, _playerBody.transform.position.z);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float MovementSpeed = 0;
+    private float _xInput;
+    private float _zInput;
+    public Rigidbody PlayerRigid;
+    public Vector3 MousePosition;
+    public Transform PlayerTransform;
+    public GameObject Bullet;
+
+    void Update () {
+        _xInput = Input.GetAxis("Horizontal") * Time.deltaTime * MovementSpeed;
+        _zInput = Input.GetAxis("Vertical") * Time.deltaTime * MovementSpeed;
+    
+        transform.Translate(_xInput, 0, _zInput);
+        faceMouse();
+    }
+
+    void faceMouse()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        mousePosition = new Vector3(mousePosition.x, .15f , mousePosition.z);
+        PlayerTransform.LookAt(mousePosition);
+    }
+}
+
